9f38a80eb3fc1f309271a88a27bba922cac98804
GUNWOO LEE <gunman@GUNWOOui-MacBook-Pro.local>
2019-04-23 11:19:17 +0900

    파라메터 비우고 시작

--- center/js/center.js
+++ center/js/center.js
@@ -183,9 +183,9 @@ function appendDesignTable(coordinates) {
   aPositions.push( {lat:lonLat[1], lon: lonLat[0], alt: 3, act: 0, id: strid} );
   var appendRow = "<tr class='odd gradeX' id='misstr_" + tableCount + "'><td>" + tableCount + "</td><td colspan=3>"
       + "<table border=0 width='100%'><tr><td class='center' bgcolor='#eee'>" + lonLat[1] + "</td><td class='center' bgcolor='#fff'> " + lonLat[0] + "</td>"
-      + "<td class='center'><input name='altdata_" + tableCount + "' id='altdata_" + tableCount + "' type='text' class='form-control' value='3'></td></tr>"
+      + "<td class='center'><input name='altdata_" + tableCount + "' id='altdata_" + tableCount + "' type='text' placeholder='Altitude (m)' class='form-control'></td></tr>"
       + "<tr>"
-      + "<td class='center'><input name='speeddata_" + tableCount + "' id='speeddata_" + tableCount + "' type='text' class='form-control' value='3'></td>"
+      + "<td class='center'><input name='speeddata_" + tableCount + "' id='speeddata_" + tableCount + "' type='text' placeholder='Speed (m/s)' class='form-control'></td>"
       + "<td class='center'>"
       + "<select class='form-control' id='actiondata_" + tableCount + "'>"
           + "<option selected value=0>STAY</option>"
@@ -196,7 +196,7 @@ function appendDesignTable(coordinates) {
           + "<option value=5>GIMBAL_PITCH</option>"
           + "<option value=7>CAMERA_ZOOM</option>"
           + "<option value=8>CAMERA_FOCUS</option>"
-      + "</select></td><td class='center'><input name='actionparam_" + tableCount + "' id='actionparam_" + tableCount + "' placeholder='action Param' type='text' class='form-control' value='0'>"
+      + "</select></td><td class='center'><input name='actionparam_" + tableCount + "' id='actionparam_" + tableCount + "' placeholder='action Param' type='text' class='form-control'>"
       + "</td></tr></table>"
   + "</td></tr>"
   $('#dataTable-points > tbody:last').append(appendRow);
