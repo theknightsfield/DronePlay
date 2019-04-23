74e8c906990d686f8c87093ae35389056018fd37
GUNWOO LEE <gunman@GUNWOOui-MacBook-Pro.local>
2019-04-23 20:23:55 +0900

    지도고정

--- center/js/center.js
+++ center/js/center.js
@@ -82,6 +82,17 @@ function designInit() {
 		mission_name = mission_name.split('&')[0];
     setDesignTableByMission(mission_name);
   }
+
+  var bannerOffset = $( '.topFixBanner' ).offset();
+  $( window ).scroll( function() {  //window에 스크롤링이 발생하면
+        if ( $( document ).scrollTop() > bannerOffset.top ) {   // 위치 및 사이즈를 파악하여 미리 정한 css class를 add 또는 remove 합니다.
+          $( '.topFixBanner' ).addClass( 'topFixBannerFixed' );
+        }
+        else {
+          $( '.topFixBanner' ).removeClass( 'topFixBannerFixed' );
+        }
+   });
+
 }
 
 function setDesignTableByMission(name) {
