165ec38e86c4bc04d783ebe7b1f3c62b6126892a
GUNWOO LEE <gunman@GUNWOOui-MacBook-Pro.local>
2019-05-01 20:23:50 +0900

    미션 디자인때 화면 크기 변경하면 포인트가 이상한 곳에 찍히는 문제 수정

--- center/js/center.js
+++ center/js/center.js
@@ -87,9 +87,11 @@ function designInit() {
   $( window ).scroll( function() {  //window에 스크롤링이 발생하면
         if ( $( document ).scrollTop() > bannerOffset.top ) {   // 위치 및 사이즈를 파악하여 미리 정한 css class를 add 또는 remove 합니다.
           $( '.topFixBanner' ).addClass( 'topFixBannerFixed' );
+          map.updateSize();
         }
         else {
           $( '.topFixBanner' ).removeClass( 'topFixBannerFixed' );
+          map.updateSize();
         }
    });
 
