8e5d72b67dcb003c79f5232088676aad815364d2
gunman <gunman97@gmail.com>
2020-06-20 12:52:35 +0900

    드로미 데이터를 파일에서 읽어오게끔 개선 2

--- center/js/dromi.js
+++ center/js/dromi.js
@@ -242,6 +242,7 @@ function showData(index) {
   }
 	
 	if (item.data == null || item.data == "") {
+		var userid = getCookie("dev_user_id");
 		var jdata = {"action": "dromi", "daction": "get", "clientid" : userid, "name" : item.name};
 	
 	  showLoader();
