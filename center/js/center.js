2c7b6c3bac025b2814e02dbb5a2e9cc6383d916b
gunman <gunman97@gmail.com>
2020-07-20 14:14:40 +0900

    [WIP] 디자인 페이지 개선중

--- center/js/center.js
+++ center/js/center.js
@@ -74,19 +74,7 @@ function monitorInit() {
 }
 
 
-function designInit() {
-	var modify = new ol.interaction.Modify({
-    features: pointSource,
-    // the SHIFT key must be pressed to delete vertices, so
-    // that new vertices can be drawn at the same position
-    // of existing vertices
-    deleteCondition: function(event) {
-      return ol.events.condition.shiftKeyOnly(event) &&
-          ol.events.condition.singleClick(event);
-    }
-  });
-  map.addInteraction(modify);
-  
+function designInit() {	 
   draw = new ol.interaction.Draw({
       source: pointSource,
       type: "Point"      
