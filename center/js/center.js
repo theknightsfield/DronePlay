8a0f25869d4895621b15eda9c04d65dbcb00dc4b
gunman <gunman97@gmail.com>
2020-07-20 17:37:13 +0900

    [WIP] 디자인 페이지 개선중

--- center/js/center.js
+++ center/js/center.js
@@ -109,6 +109,7 @@ function designInit() {
     setDesignTableByMission(mission_name);
   }
 
+	/*
   var bannerOffset = $( '.topFixBanner' ).offset();
   $( window ).scroll( function() {  //window에 스크롤링이 발생하면
         if ( $( document ).scrollTop() > bannerOffset.top ) {   // 위치 및 사이즈를 파악하여 미리 정한 css class를 add 또는 remove 합니다.
@@ -120,6 +121,7 @@ function designInit() {
           map.updateSize();
         }
    });
+  */
    
   initSlider(1);
 }
