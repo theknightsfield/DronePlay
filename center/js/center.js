be619ffe795ae7690ffc8c8da443eccfbf44ec34
gunman <gunman97@gmail.com>
2020-07-23 19:18:13 +0900

    [WIP] 지도 클릭시 지적도 표시 추가중

--- center/js/center.js
+++ center/js/center.js
@@ -827,15 +827,80 @@ function makeForFlightListMap(index, flat, flng) {
 
   var vSource = new ol.source.Vector();
   
-  var styleFunction = function(feature) {       
-      var retStyle =   new ol.style.Style({
-          stroke: new ol.style.Stroke({ 
-            color: "#ff0000",
-            width: 5
-          })
-        });
-       	return retStyle;
-      };
+  var image = new ol.style.Circle({
+	  radius: 5,
+	  fill: null,
+	  stroke: new ol.style.Stroke({ color: 'red', width: 1 })
+	});
+	
+	var styles = {
+	  'Point': new ol.style.Style({
+	    image: image
+	  }),
+	  'LineString': new ol.style.Style({
+	    stroke: new ol.style.Stroke({
+	      color: 'green',
+	      width: 1
+	    })
+	  }),
+	  'MultiLineString': new ol.style.Style({
+	    stroke: new ol.style.Stroke({
+	      color: 'green',
+	      width: 1
+	    })
+	  }),
+	  'MultiPoint': new ol.style.Style({
+	    image: image
+	  }),
+	  'MultiPolygon': new ol.style.Style({
+	    stroke: new ol.style.Stroke({
+	      color: 'yellow',
+	      width: 1
+	    }),
+	    fill: new ol.style.Fill({
+	      color: 'rgba(255, 255, 0, 0.1)'
+	    })
+	  }),
+	  'Polygon': new ol.style.Style({
+	    stroke: new ol.style.Stroke({
+	      color: 'blue',
+	      lineDash: [4],
+	      width: 3
+	    }),
+	    fill: new ol.style.Fill({
+	      color: 'rgba(0, 0, 255, 0.1)'
+	    })
+	  }),
+	  'GeometryCollection': new ol.style.Style({
+	    stroke: new ol.style.Stroke({
+	      color: 'magenta',
+	      width: 2
+	    }),
+	    fill: new ol.style.Fill({
+	      color: 'magenta'
+	    }),
+	    image: new ol.style.Circle({
+	      radius: 10,
+	      fill: null,
+	      stroke: new ol.style.Stroke({
+	        color: 'magenta'
+	      })
+	    })
+	  }),
+	  'Circle': new ol.style.Style({
+	    stroke: new ol.style.Stroke({
+	      color: 'red',
+	      width: 2
+	    }),
+	    fill: new ol.style.Fill({
+	      color: 'rgba(255,0,0,0.2)'
+	    })
+	  })
+	};
+	
+	var styleFunction = function (feature) {
+	  return styles[feature.getGeometry().getType()];
+	};
   
   var vVectorLayer = new ol.layer.Vector({
       source: vSource,
