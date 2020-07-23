05bcc06452d3771cb6f59903da53ee86988d5849
gunman <gunman97@gmail.com>
2020-07-23 19:14:09 +0900

    [WIP] 지도 클릭시 지적도 표시 추가중

--- center/js/center.js
+++ center/js/center.js
@@ -827,42 +827,20 @@ function makeForFlightListMap(index, flat, flng) {
 
   var vSource = new ol.source.Vector();
   
-  var styles = [
-	  /* We are using two different styles for the polygons:
-	   *  - The first style is for the polygons themselves.
-	   *  - The second style is to draw the vertices of the polygons.
-	   *    In a custom `geometry` function the vertices of a polygon are
-	   *    returned as `MultiPoint` geometry, which will be used to render
-	   *    the style.
-	   */
-	  new Style({
-	    stroke: new Stroke({
-	      color: 'blue',
-	      width: 3
-	    }),
-	    fill: new Fill({
-	      color: 'rgba(0, 0, 255, 0.1)'
-	    })
-	  }),
-	  new Style({
-	    image: new CircleStyle({
-	      radius: 5,
-	      fill: new Fill({
-	        color: 'orange'
-	      })
-	    }),
-	    geometry: function(feature) {
-	      // return the coordinates of the first ring of the polygon
-	      var coordinates = feature.getGeometry().getCoordinates()[0];
-	      return new MultiPoint(coordinates);
-	    }
-	  })
-	];
+  var styleFunction = function(feature) {       
+      var retStyle =   new ol.style.Style({
+          stroke: new ol.style.Stroke({ 
+            color: "#ff0000",
+            width: 5
+          })
+        });
+       	return retStyle;
+      };
   
   var vVectorLayer = new ol.layer.Vector({
       source: vSource,
       zIndex: 10000,
-      style: styles
+      style: styleFunction
     });    
 
     
